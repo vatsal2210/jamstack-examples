e64b165c0b8890c1a348244beb6aa6eef8b0bf09
Vatsal Shah <vatsalshah2210@gmail.com>
2022-07-07 12:30:13 -0400

    Localized News - CDN integrations

---
+++ 5-LOCALIZED-NEWS/.eleventy.js
@@ -0,0 +1,13 @@
+module.exports = (config) => {
+  // types of files or assets to pass to "dist"
+
+  config.addPassthroughCopy("src/js");
+
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+      data: "_data",
+    },
+  };
+};
---
+++ 5-LOCALIZED-NEWS/src/_data/news.js
@@ -0,0 +1,25 @@
+const axios = require("axios").default;
+const countries = require("./countries.json");
+require("dotenv").config();
+
+async function getNews(country) {
+  try {
+    const response = await axios.get(
+      `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.API_KEY}&pageSize=5`
+    );
+    return {
+      country: country,
+      articles: response.data.articles,
+    };
+  } catch (error) {
+    console.error("Found Error", error);
+  }
+}
+
+module.exports = async function () {
+  var newsPromises = countries.map(getNews);
+  return Promise.all(newsPromises).then((obj) => {
+    console.log(obj);
+    return obj;
+  });
+};
